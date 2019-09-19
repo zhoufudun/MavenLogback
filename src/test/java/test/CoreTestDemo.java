b79bfeb45a6b03853b64a5e8786330e207bc8067
zhoufudun <1215906004@qq.com>
2019-09-19 11:30:30 +0800

    logback

---
+++ src/test/java/test/CoreTestDemo.java
@@ -0,0 +1,14 @@
+package test;
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.baseJunit4.BaseJunit4Test;
+
+public class CoreTestDemo extends BaseJunit4Test {
+	//ÄãµÄ²âÊÔ´úÂë
+}
